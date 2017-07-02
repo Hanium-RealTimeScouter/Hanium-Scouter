e6ce459e7b571e3874130f88ad71253b8906e259
S.J. Lee <occidere@naver.com>
2017-07-02 12:29:31 +0900

    hanium scouter project alert modules commit

--- modules/scouter-plugin-server-alert-email/src/scouter/plugin/server/alert/email/EmailPlugin.java
+++ modules/scouter-plugin-server-alert-email/src/scouter/plugin/server/alert/email/EmailPlugin.java
@@ -1,4 +1,4 @@
-﻿/*
+/*
  *  Copyright 2016 Scouter Project.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); 
@@ -30,6 +30,7 @@ import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.SimpleEmail;
 
+import hanium.util.ObjectContainer;
 import scouter.lang.AlertLevel;
 import scouter.lang.TextTypes;
 import scouter.lang.TimeTypeEnum;
@@ -38,9 +39,7 @@ import scouter.lang.pack.AlertPack;
 import scouter.lang.pack.MapPack;
 import scouter.lang.pack.ObjectPack;
 import scouter.lang.pack.PerfCounterPack;
-import scouter.lang.pack.SummaryPack;
 import scouter.lang.pack.XLogPack;
-import scouter.lang.pack.XLogProfilePack;
 import scouter.lang.plugin.PluginConstants;
 import scouter.lang.plugin.annotation.ServerPlugin;
 import scouter.net.RequestCmd;
@@ -82,15 +81,15 @@ public class EmailPlugin {
 								ObjectPack objectPack = AgentManager.getAgent(objHash);
 								MapPack mapPack = new MapPack();
 				            	mapPack.put("objHash", objHash);
-				            	
 								mapPack = AgentCall.call(objectPack, RequestCmd.OBJECT_THREAD_LIST, mapPack);
 								
-			        			final int LIMIT = 30; //직접 만든 임계치. 평균 20 정도 나오는 것으로 보아 30 정도가 적절
+								final int LIMIT = 30; //직접 만든 임계치. 평균 20 정도 나오는 것으로 보아 30 정도가 적절
 								
 								//스레드 카운트 한계치. 여기서 지정한 숫자를 넘기면 thread exceed threshold 관련 메일을 보낸다.
 				        		int threadCountThreshold = conf.getInt("ext_plugin_thread_count_threshold", LIMIT);
 				        		int threadCount = mapPack.getList("name").size();
 				        		
+				        		/* 스레드 갯수 체크 */
 				        		if (threadCountThreshold != 0 && threadCount > threadCountThreshold) {
 				        			AlertPack ap = new AlertPack();
 
@@ -98,13 +97,15 @@ public class EmailPlugin {
 				        			if(threadCount > threadCountThreshold * 2) ap.level = AlertLevel.FATAL;
 				        			else ap.level = AlertLevel.WARN;
 				        			
-				    		        ap.objHash = objHash;
+				        			ap.objHash = objHash;
 				    		        ap.title = "스레드가 너무 많습니다!";
 				    		        ap.message = objectPack.objName + "의 스레드 개수가 " + threadCount + "개로, 임계치("+LIMIT+"개)를 초과하였습니다!";
 				    		        ap.time = System.currentTimeMillis();
 				    		        ap.objType = objectPack.objType;
 				    				
-				    		        alert(ap);
+				    		        long limitTime = conf.getLong("thread_limit_time", 120000);
+				    		        long safeTime = conf.getLong("thread_safe_time", 180000);
+				    		        if(ObjectContainer.sendAlert(ap, limitTime, safeTime)) alert(ap);
 				        		}
 							}
 						} catch (Exception e) {
@@ -117,46 +118,107 @@ public class EmailPlugin {
     	}
 	}
 
-    //실질적으로 메일을 보내는 역할을 하는 메서드
+    /**
+     * 에러 발생시 메일을 보내는 메서드<br>
+     * 지정한 임계시간 초과시에만 메일을 보내도록 설정<br>
+     * @param pack 에러발생 객체를 담고 있는 pack
+     */
     @ServerPlugin(PluginConstants.PLUGIN_SERVER_ALERT)
     public void alert(final AlertPack pack) {
-        if (conf.getBoolean("ext_plugin_email_send_alert", true)) { //email 발송 여부
+    	/* Email 발송 여부 결정 */
+        if (conf.getBoolean("ext_plugin_email_send_alert", true)) {
         	
-        	// Get log level (0 : INFO, 1 : WARN, 2 : ERROR, 3 : FATAL)
+        	/* 
+        	 * Get log level (0 : INFO, 1 : WARN, 2 : ERROR, 3 : FATAL)
+        	 * 0으로 설정하면 모든 행동에 대해 메일이 보내지게 된다.
+        	 */
         	int level = conf.getInt("ext_plugin_email_level", 0);
         	
+        	/* 위에서 지정한 level 값 수준보다 낮은 에러는 메일 안보냄 */
         	if (level <= pack.level) {
         		new Thread() {
         			public void run() {
                         try {
-                        	// Get server configurations for email
-                            String hostname = conf.getValue("ext_plugin_email_smtp_hostname", "smtp.gmail.com"); //smtp 지정
-                            int port = conf.getInt("ext_plugin_email_smtp_port", 587); //smtp 포트번호
-                            String username = conf.getValue("ext_plugin_email_username", "haniumscouter@gmail.com"); //발신자 이메일 계정
-                            String password = conf.getValue("ext_plugin_email_password", "비밀번호"); //발신자 이메일 비밀번호
-                            boolean tlsEnabled = conf.getBoolean("ext_plugin_email_tls_enabled", true); //tls 여부
-                            String from = conf.getValue("ext_plugin_email_from_address", "haniumscouter@gmail.com"); //수신자에게 표시될 이메일 정보
-                            String to = conf.getValue("ext_plugin_email_to_address", "occidere@naver.com"); //수신자 이메일(,로 구분)
-                            String cc = conf.getValue("ext_plugin_email_cc_address"); //cc 이메일
                         	
-                        	assert hostname != null;
-                        	assert port > 0;
-                        	assert username != null;
-                        	assert password != null;
-                        	assert from != null;
-                        	assert to != null;
+//                        	/************** 일정 시간 경과 시 메일 보내게 하는 코드 ************/
+//                        	String additionalMessage = "";
+//                        	long values[], curTime = pack.time; //현재시간;
+//                        	final long A_MINUTE = 60000; //1분
+//                        	int tmpHash = pack.objHash; //객체의 에러 발생 시간
+//                        	
+//                        	//본문에 표시될 에러 발생 시간 포맷
+//                        	SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초");
+//                        	
+//                        	/* 객체의 에러 발생 임계치 초과 여부 체크 */
+//                        	if(ObjectContainer.contains(tmpHash)){
+//
+//                        		values = ObjectContainer.getAllValues(tmpHash); //해당 오브젝트의 값
+//                        		
+//                        		//임계시간 초과 시 메세지 추가 후 메일 전송
+//                        		if(ObjectContainer.exceedLimitTime(tmpHash)){
+//                        			
+//                        			additionalMessage = String.format(
+//                        					"[위 험] : 에러가 %d분 동안 지속 발생중입니다! (제한시간: %d분)\n"+
+//                        					"          최초 발생시간: %s\n"+
+//                        					"          누적 에러 발생 횟수: %d 회\n",
+//                        					(curTime - values[ObjectContainer.ERR_START_TIME]) / A_MINUTE,
+//                        					(values[ObjectContainer.ERR_LIMIT_TIME]) / A_MINUTE, 
+//                        					sdf.format(new Date(values[ObjectContainer.ERR_START_TIME])),
+//                        					values[ObjectContainer.ERR_COUNT]);
+//                        			
+//                        			/* 임계시간 초과 시 메일 보내고 객체 정보 초기화 -> 정책별로 상이 */
+//                        			//ObjectContainer.remove(tmpHash);
+//                        			ObjectContainer.setValue(tmpHash, ObjectContainer.ERR_START_TIME, curTime);
+//                        		}
+//                        		else{ 
+//                        			//아직 임계시간 안지났으면 에러발생횟수 + 1 갱신,
+//                        			//마지막 에러 발생시간 갱신 후 종료
+//                        			ObjectContainer.setValue(tmpHash, ObjectContainer.ERR_COUNT, values[ObjectContainer.ERR_COUNT] + 1);
+//                        			ObjectContainer.setValue(tmpHash, ObjectContainer.ERR_LAST_TIME, curTime);
+//                        			return;
+//                        		}
+//                        	}
+//                        	else{ //객체가 없다 == 첫 에러 발생
+//                        		
+//                        		//테스트용 임계시간 2분, 안정시간 3분
+//                        		// 2분동안 계속 에러나면 메일보낸다
+//                        		// 3분간 에러가 한번도 없으면 객체 삭제
+//                        		ObjectContainer.init(tmpHash, curTime, 2*A_MINUTE, 3*A_MINUTE);
+//                        		return; //첫 에러 발생에 대한 정보 등록 후 종료
+//                        	}
+//                        	/********************************************************************/
+                        	
+                        	
+							// Get server configurations for email
+							String hostname = conf.getValue("ext_plugin_email_smtp_hostname", "smtp.gmail.com"); // smtp 지정
+							int port = conf.getInt("ext_plugin_email_smtp_port", 587); // smtp 포트번호
+							
+							String username = conf.getValue("ext_plugin_email_username", "haniumscouter@gmail.com"); // 발신자 이메일 계정
+							String password = conf.getValue("ext_plugin_email_password", "dkqorhvk!@#$"); // 발신자 이메일 비밀번호
+							
+							boolean tlsEnabled = conf.getBoolean("ext_plugin_email_tls_enabled", true); // tls 여부(gamil은 true)
+							
+							String from = conf.getValue("ext_plugin_email_from_address", "haniumscouter@gmail.com"); // 수신자에게 표시될 이메일 정보
+							String to = conf.getValue("ext_plugin_email_to_address", "occidere@naver.com"); // 수신자 이메일(공백없이 ,로 구분)
+							String cc = conf.getValue("ext_plugin_email_cc_address"); // cc 이메일
+
+							assert hostname != null;
+							assert port > 0;
+							assert username != null;
+							assert password != null;
+							assert from != null;
+							assert to != null;
                         	
                         	// Get agent Name. 건들지 말자.
                         	String name = AgentManager.getAgentName(pack.objHash) == null ? "N/A" : AgentManager.getAgentName(pack.objHash);
                         	
-                        	if (name.equals("N/A") && pack.message.endsWith("connected.")) {
-                    			int idx = pack.message.indexOf("connected");
-                        		if (pack.message.indexOf("reconnected") > -1) {
-                        			name = pack.message.substring(0, idx - 6);
-                        		} else {
-                        			name = pack.message.substring(0, idx - 4);
-                        		}
-                        	}
+							if (name.equals("N/A") && pack.message.endsWith("connected.")) {
+								int idx = pack.message.indexOf("connected");
+								if (pack.message.indexOf("reconnected") > -1)
+									name = pack.message.substring(0, idx - 6);
+								else
+									name = pack.message.substring(0, idx - 4);
+							}
                         	
                         	// Make email subject
                             String subject = "[" + AlertLevel.getName(pack.level) + "] " + pack.objType.toUpperCase() + 
@@ -165,18 +227,19 @@ public class EmailPlugin {
                             String title = pack.title;
                             String msg = pack.message;
                             
-                            /* Agent 객체가 죽으면(inactivate) 자체로 메일 보냄
+                            /* 
+                             * Agent 객체가 죽으면(inactivate) 자체로 메일 보냄.
                              * 애초에 가비지 컬렉션, 요청 응답시간 등은 Agent가 죽어버리면 의미가 없기 때문에
-                             * Agent 사망 시 title을 포함한 메세지 정보를 싹 바꿔버림
+                             * Agent 사망 시 title을 포함한 메세지 정보를 싹 바꿔버림.
                              */
                             if (title.equals("INACTIVE_OBJECT")) {
-                            	title = "Object가 비활성화(Inactivated) 되었습니다!";
+                            	title = name + " 가 비활성화(Inactivated) 되었습니다!";
                             	msg = pack.message.substring(0, pack.message.indexOf("OBJECT") - 1);
                             }
                             
-                            //테스트용 에러 발생 시간
-                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초");
-
+                            //본문에 표시될 에러 발생 시간 포맷
+                        	SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초");
+                            
                             // Make email message
                             String message = "[한이음 실시간 성능 모니터링 시스템 알림]\n\n" +
                             				 "[제 목] : " + title + "\n" + 
@@ -184,7 +247,7 @@ public class EmailPlugin {
                             				 "[종 류] : " + pack.objType.toUpperCase() + "\n" + 
                                           	 "[이 름] : " + name + "\n" + 
                                           	 "[수 준] : " + AlertLevel.getName(pack.level) + "\n" +
-                                          	 "[내 용] : " + msg;
+                                          	 "[내 용] : " + msg +"\r\n";
                                           
                             // Create an Email instance
                             Email email = new SimpleEmail();
@@ -204,9 +267,7 @@ public class EmailPlugin {
                             
                             //cc가 있으면 , 를 기준으로 분리
                             if (cc != null) {
-                            	for (String addr : cc.split(",")) {
-                            		email.addCc(addr);
-                            	}
+                            	for (String addr : cc.split(",")) email.addCc(addr);
                             }
                             
                             // Send the email
@@ -236,30 +297,31 @@ public class EmailPlugin {
 			if (op == null && pack.wakeup == 0L) {
 				// in case of new agent connected
 				ap = new AlertPack();
-		        ap.level = AlertLevel.INFO;
-		        ap.objHash = pack.objHash;
-		        ap.title = "Object 활성화 감지!";
-		        ap.message = pack.objName + "가 연결되었습니다.";
-		        ap.time = System.currentTimeMillis();
-		        
-		        if (AgentManager.getAgent(pack.objHash) != null) {
-		        	ap.objType = AgentManager.getAgent(pack.objHash).objType;
-		        } else {
-		        	ap.objType = "scouter";
-		        }
-				
-		        alert(ap);
-	    	} else if (op.alive == false) {
+				ap.level = AlertLevel.INFO;
+				ap.objHash = pack.objHash;
+				ap.title = "Object 활성화 감지!";
+				ap.message = pack.objName + "가 연결되었습니다.";
+				ap.time = System.currentTimeMillis();
+
+				if (AgentManager.getAgent(pack.objHash) != null) {
+					ap.objType = AgentManager.getAgent(pack.objHash).objType;
+				} else {
+					ap.objType = "scouter";
+				}
+
+				alert(ap);
+
+			} else if (op.alive == false) {
 				// in case of agent reconnected
 				ap = new AlertPack();
-		        ap.level = AlertLevel.INFO;
-		        ap.objHash = pack.objHash;
-		        ap.title = "Object가 활성화되었습니다.";
-		        ap.message = pack.objName + "가 재연결되었습니다.";
-		        ap.time = System.currentTimeMillis();
-		        ap.objType = AgentManager.getAgent(pack.objHash).objType;
-				
-		        alert(ap);
+				ap.level = AlertLevel.INFO;
+				ap.objHash = pack.objHash;
+				ap.title = "Object가 활성화되었습니다.";
+				ap.message = pack.objName + "가 재연결되었습니다.";
+				ap.time = System.currentTimeMillis();
+				ap.objType = AgentManager.getAgent(pack.objHash).objType;
+
+				alert(ap);
 	    	}
 			// inactive state can be handled in alert() method.
 			// Agent의 비활성화(inactivate)시 메일을 보내는 기능은 alert() 메서드 내부에 직접 구현되어 있음.
@@ -268,55 +330,57 @@ public class EmailPlugin {
 	
     
 	//요청시간이 임계치를 넘으면 메일을 보낸다.
-    @ServerPlugin(PluginConstants.PLUGIN_SERVER_XLOG)
-    public void xlog(XLogPack pack) {
-    	try {
-    		final int LIMIT = 4000; //직접 만든 임계치. 응답시간 임계치는 4초가 적당하다.
-    		
-    		//요청 경과시간이 여기서 정한 시간(ms)를 넘기면 메일을 보낸다.
-    		int elapsedThreshold = conf.getInt("ext_plugin_elapsed_time_threshold", LIMIT);
-    		
-    		if (elapsedThreshold != 0 && pack.elapsed > elapsedThreshold) {
-    			String serviceName = TextRD.getString(DateUtil.yyyymmdd(pack.endTime), TextTypes.SERVICE, pack.service);
+	@ServerPlugin(PluginConstants.PLUGIN_SERVER_XLOG)
+	public void xlog(XLogPack pack) {
+		try {
+			final int LIMIT = 2000; // 직접 만든 임계치. 응답시간 임계치는 4초가 적당하다.
+
+			// 요청 경과시간이 여기서 정한 시간(ms)를 넘기면 메일을 보낸다.
+			int elapsedThreshold = conf.getInt("ext_plugin_elapsed_time_threshold", LIMIT);
+
+			if (elapsedThreshold != 0 && pack.elapsed > elapsedThreshold) {
+				String serviceName = TextRD.getString(DateUtil.yyyymmdd(pack.endTime), TextTypes.SERVICE, pack.service);
+
+				AlertPack ap = new AlertPack();
     			
-    			AlertPack ap = new AlertPack();
-    			
-		        ap.level = AlertLevel.WARN;
-		        ap.objHash = pack.objHash;
-		        ap.title = "요청 응답시간이 너무 느립니다!";
+				ap.level = AlertLevel.WARN;
+				ap.objHash = pack.objHash;
+				ap.title = "요청 응답시간이 너무 느립니다!";
 		        ap.message = "[" + AgentManager.getAgentName(pack.objHash) + "] " 
 		        				+ pack.service + "(" + serviceName + ") "
 		        				+ "에 대한 요청 응답시간이 " + pack.elapsed + "ms로, 임계치(" + LIMIT + "ms)를 초과하였습니다.";
-		        ap.time = System.currentTimeMillis();
-		        ap.objType = AgentManager.getAgent(pack.objHash).objType;
-				
-		        alert(ap);
-    		}
-    		
+				ap.time = System.currentTimeMillis();
+				ap.objType = AgentManager.getAgent(pack.objHash).objType;
+
+				long limitTime = conf.getLong("elapsed_limit_time", 60000);
+		        long safeTime = conf.getLong("elapsed_safe_time", 120000);
+		        if(ObjectContainer.sendAlert(ap, limitTime, safeTime)) alert(ap);
+			}
+
 		} catch (Exception e) {
 			Logger.printStackTrace(e);
 		}
-    }
+	}
 
-    //가비지 컬렉션 시간이 임계치를 초과하면 메일을 보낸다.
-    @ServerPlugin(PluginConstants.PLUGIN_SERVER_COUNTER)
-    public void counter(PerfCounterPack pack) {
-        String objName = pack.objName;
-        int objHash = HashUtil.hash(objName);
-        String objType = null;
-        String objFamily = null;
+	// 가비지 컬렉션 시간이 임계치를 초과하면 메일을 보낸다.
+	@ServerPlugin(PluginConstants.PLUGIN_SERVER_COUNTER)
+	public void counter(PerfCounterPack pack) {
+		String objName = pack.objName;
+		int objHash = HashUtil.hash(objName);
+		String objType = null;
+		String objFamily = null;
 
-        if (AgentManager.getAgent(objHash) != null) {
-        	objType = AgentManager.getAgent(objHash).objType;
-        }
-        
-        if (objType != null) {
-        	objFamily = CounterManager.getInstance().getCounterEngine().getObjectType(objType).getFamily().getName();
-        }
-        
-        try {
-	        // in case of objFamily is javaee
-	        if (CounterConstants.FAMILY_JAVAEE.equals(objFamily)) {
+		if (AgentManager.getAgent(objHash) != null) {
+			objType = AgentManager.getAgent(objHash).objType;
+		}
+
+		if (objType != null) {
+			objFamily = CounterManager.getInstance().getCounterEngine().getObjectType(objType).getFamily().getName();
+		}
+
+		try {
+			// in case of objFamily is javaee
+			if (CounterConstants.FAMILY_JAVAEE.equals(objFamily)) {
 	        	// save javaee type's objHash
 	        	if (!javaeeObjHashList.contains(objHash)) {
 	        		javaeeObjHashList.add(objHash);
@@ -339,8 +403,10 @@ public class EmailPlugin {
 	    		        ap.message = objName + "의 GC time이 " + gcTime + "ms로, 임계치("+LIMIT+"ms)를 초과하였습니다.";
 	    		        ap.time = System.currentTimeMillis();
 	    		        ap.objType = objType;
-	    				
-	    		        alert(ap);
+
+	    		        long limitTime = conf.getLong("gc_time_limit_time", 100);
+	    		        long safeTime = conf.getLong("gc_time_safe_time", 200);
+	    		        if(ObjectContainer.sendAlert(ap, limitTime, safeTime)) alert(ap);
 	        		}
 	        	}
 	    	}
